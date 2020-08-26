75dd63dab7ccb855dff02a84b86f65511a892c82
asaki kusano <darumade4@gmail.com>
2020-08-26 22:59:20 +0900

    init

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def user_mail(user)
     @user = user
-    mail to: "u_test@ttt.com", subject: "user作成完了通知メール"
+    mail to: "darumade4@icloud.com", subject: "user作成完了通知メール"
   end
 end
